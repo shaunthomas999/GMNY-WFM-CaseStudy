53b0dd9637a0c11d2fcdfcc797d010aa78ae3dbb
MRenka <marcel.renka@googlemail.com>
2015-05-29 15:00:23 +0200

    Loan Approval without .metadata folder

---
+++ Loan Approval/src/main/java/org/camunda/bpm/getstarted/loanapproval/LoanApprovalApplication.java
@@ -0,0 +1,9 @@
+package org.camunda.bpm.getstarted.loanapproval;
+ 
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+ 
+@ProcessApplication("Loan Approval App")
+public class LoanApprovalApplication extends ServletProcessApplication {
+  // empty implementation
+}
