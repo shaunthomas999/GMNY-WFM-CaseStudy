6f4cf4541fbff77d1ef3a5e502657af1ded1a61f
ChrisJim <christoph.wemmer@gmail.com>
2015-06-07 18:04:54 +0200

    deleted old folder

--- Loan Approval/loan-approval/src/main/java/org/camunda/bpm/getstarted/loanapproval/LoanApprovalApplication.java
+++
@@ -1,9 +0,0 @@
-package org.camunda.bpm.getstarted.loanapproval;
- 
-import org.camunda.bpm.application.ProcessApplication;
-import org.camunda.bpm.application.impl.ServletProcessApplication;
- 
-@ProcessApplication("Loan Approval App")
-public class LoanApprovalApplication extends ServletProcessApplication {
-  // empty implementation
-}
