d2bb82e915a6448edd47b15efc34e5d4f5da3f48
Shaun Thomas <shaunthomas999@gmail.com>
2015-06-12 01:13:08 +0200

    Completed dynamic allocation of user to task

--- task-assignment-example/src/main/java/org/camunda/bpm/getstarted/CustomerServiceAssignmentHandler.java
+++ task-assignment-example/src/main/java/org/camunda/bpm/getstarted/CustomerServiceAssignmentHandler.java
@@ -1,17 +1,16 @@
 package org.camunda.bpm.getstarted;
 
 import java.util.List;
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.camunda.bpm.engine.IdentityService;
 import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.engine.ProcessEngines;
-import org.camunda.bpm.engine.TaskService;
 import org.camunda.bpm.engine.delegate.DelegateTask;
 import org.camunda.bpm.engine.delegate.TaskListener;
-import org.camunda.bpm.engine.identity.Group;
-import org.camunda.bpm.engine.task.Task;
+import org.camunda.bpm.engine.identity.User;
 
 public class CustomerServiceAssignmentHandler implements TaskListener {
 	
@@ -19,13 +18,19 @@ public class CustomerServiceAssignmentHandler implements TaskListener {
 	 
 	public void notify(DelegateTask delegateTask) {
 		LOGGER.log(Level.INFO, "Inside notify method");
+		
 		ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
-		TaskService taskService = processEngine.getTaskService();
-		List<Task> tasks = taskService.createTaskQuery().taskCandidateGroup("customerService").list();
-		LOGGER.info("Tasks : " + tasks);
 		IdentityService identityService = processEngine.getIdentityService();
-		List<Group> groups = identityService.createGroupQuery().groupId("customerService").list();
-		LOGGER.info("Groups : " + groups);
+		
+		List<User> users = identityService.createUserQuery().memberOfGroup("customerService").list();
+		LOGGER.info("Users : " + users);
+		
+		Random randomizer = new Random();
+		User selectedUser = users.get(randomizer.nextInt(users.size()));
+		LOGGER.info("Selected user : " + selectedUser);
+		
+		delegateTask.setAssignee(selectedUser.getId());
+		LOGGER.info("User assigned to task");
 	}
 
 }
