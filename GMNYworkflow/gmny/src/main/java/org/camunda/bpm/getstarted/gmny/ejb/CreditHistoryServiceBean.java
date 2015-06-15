8668c03a6f75501a59f30ccff250c725adaedba5
ChrisJim <christoph.wemmer@gmail.com>
2015-06-15 12:24:22 +0200

    created GTA simultion, implemented HTTP POST to GTA, other smaller edits

--- GMNYworkflow/gmny/src/main/java/org/camunda/bpm/getstarted/gmny/ejb/CreditHistoryServiceBean.java
+++ GMNYworkflow/gmny/src/main/java/org/camunda/bpm/getstarted/gmny/ejb/CreditHistoryServiceBean.java
@@ -5,16 +5,28 @@ import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.getstarted.gmny.model.CreditHistoryEntity;
 import org.camunda.bpm.getstarted.gmny.service.CreditHistoryService;
 
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
+import com.sun.jersey.api.json.JSONConfiguration;
+import com.sun.jersey.core.util.MultivaluedMapImpl;
+
 import javax.ejb.Stateless;
 //import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.core.MediaType;
 
 import java.io.IOException;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+
 
 @Stateless
 @Named
@@ -55,4 +67,37 @@ public class CreditHistoryServiceBean implements CreditHistoryService{
 		// TODO Auto-generated method stub
 		
 	}
+	
+	public void requestCreditHistory(DelegateExecution delegateExecution) {
+		
+		try {
+			// Get all process variables
+		    Map<String, Object> variables = delegateExecution.getVariables();
+		      
+		    String message = "{\"variables\": {"
+		    		+ "\"firstname\" : {\"value\" : \"" + variables.get("firstname") + "\", \"type\": \"String\"},"
+		    		+ "\"lastname\" : {\"value\" : \"" + variables.get("lastname") + "\", \"type\": \"String\"}"
+		    		+ "} }";
+	        
+			Client client = Client.create();
+		    WebResource webResource = client.resource("http://localhost:8080/engine-rest/process-definition/key/APIsimulation/start");
+		    
+		    System.out.println("API call to GTA");
+		    ClientResponse response = webResource.type(MediaType.APPLICATION_JSON).post(ClientResponse.class, message);
+		    
+		    // check response status code
+	        if (response.getStatus() != 200) {
+	            throw new RuntimeException("Failed : HTTP error code : "
+	                    + response.getStatus());
+	        }
+	
+	        // display response
+	        String output = response.getEntity(String.class);
+	        System.out.println("Output from Server .... ");
+	        System.out.println(output + "\n");
+		} catch (Exception e) {
+        e.printStackTrace();
+        }
+	}
+	
 }
--- GMNYworkflow/gmny/src/main/java/org/camunda/bpm/getstarted/gmny/service/CreditHistoryService.java
+++ GMNYworkflow/gmny/src/main/java/org/camunda/bpm/getstarted/gmny/service/CreditHistoryService.java
@@ -3,10 +3,13 @@ package org.camunda.bpm.getstarted.gmny.service;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.getstarted.gmny.model.CreditHistoryEntity;
 
+import com.sun.jersey.api.client.ClientResponse;
+
 public interface CreditHistoryService {
 
 	public void persistCreditHistory(DelegateExecution delegateExecution);
 	
 	public void loadCreditHistory(DelegateExecution delegateExecution);
 	
+	public void requestCreditHistory(DelegateExecution delegateExecution);
 }
