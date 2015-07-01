093e6fbd1c1d370c8179de8ba2d11fc2effc3c2e
ChrisJim <christoph.wemmer@gmail.com>
2015-07-01 18:17:52 +0200

    hello world

---
+++ GMNYworkflow/gmny/src/main/java/org/camunda/bpm/getstarted/gmny/api/HelloWorld.java
@@ -0,0 +1,18 @@
+package org.camunda.bpm.getstarted.gmny.api;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+@Path("")
+public class HelloWorld {
+
+    @GET
+    @Path("/helloworld")
+    public Response getHelloWorld() {
+
+        String value = "Hello World - YEAH";
+
+        return Response.status(200).entity(value).build();
+    }
+}
