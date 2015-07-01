6d4df6e7642386f1ed8960ed05b2a64676b3050e
DanielRautschka <daniel@rautschka.com>
2015-07-01 16:43:10 +0200

    more changes

--- GMNYworkflow/gmny/src/main/java/org/camunda/bpm/getstarted/gmny/ejb/PdfServiceBean.java
+++ GMNYworkflow/gmny/src/main/java/org/camunda/bpm/getstarted/gmny/ejb/PdfServiceBean.java
@@ -22,6 +22,7 @@ import com.itextpdf.text.pdf.BaseFont;
 import com.itextpdf.text.pdf.ColumnText;
 import com.itextpdf.text.pdf.PdfContentByte;
 import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.pdf.parser.Line;
 
 import java.awt.Color;
 import java.io.FileOutputStream;
@@ -41,16 +42,21 @@ public class PdfServiceBean {
 	
 	public static void createWelcome(CustomerEntity customer) {
 
-		Document document = new Document(PageSize.A4, Utilities.millimetersToPoints(20), Utilities.millimetersToPoints(20), Utilities.millimetersToPoints(115), Utilities.millimetersToPoints(10));
-		Font fontAddressHeader = new Font(FontFamily.HELVETICA, 7, Font.UNDERLINE, new BaseColor(0, 0, 0));
-		Font fontAddressBody = new Font(FontFamily.HELVETICA, 9, Font.NORMAL, new BaseColor(0, 0, 0));
+		Document document = new Document(PageSize.A4, Utilities.millimetersToPoints(20), Utilities.millimetersToPoints(22), Utilities.millimetersToPoints(25), Utilities.millimetersToPoints(10));
+		Font fontAddressHeader = new Font(FontFamily.HELVETICA, 8, Font.UNDERLINE, new BaseColor(0, 0, 0));
+		Font fontAddressBody = new Font(FontFamily.HELVETICA, 10, Font.NORMAL, new BaseColor(0, 0, 0));
 		Font fontGmnyClaim = new Font(FontFamily.HELVETICA, 12, Font.ITALIC, new BaseColor(0, 0, 0));
 		Font fontGmnyHead = new Font(FontFamily.HELVETICA, 13, Font.BOLD, new BaseColor(0, 0, 0));
 		Font fontGmnyAddress = new Font(FontFamily.HELVETICA, 10, Font.NORMAL, new BaseColor(0, 0, 0));
 		Font fontGmnyAddressSmall = new Font(FontFamily.HELVETICA, 9, Font.ITALIC, new BaseColor(0, 0, 0));
 		
+		Font fontHeadline = new Font(FontFamily.HELVETICA, 12, Font.BOLD, new BaseColor(0, 0, 0));
 		Font fontText = new Font(FontFamily.HELVETICA, 10, Font.NORMAL, new BaseColor(0, 0, 0));
 		Font fontTextBold = new Font(FontFamily.HELVETICA, 10, Font.BOLD, new BaseColor(0, 0, 0));
+		
+		Font fontTextUnderline = new Font(FontFamily.HELVETICA, 11, Font.UNDERLINE, new BaseColor(0, 0, 0));
+		
+		Font fontTextMassive = new Font(FontFamily.HELVETICA, 22, Font.BOLD, new BaseColor(0, 0, 0));
 		
         try {
             PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(customer.getId() + "_" + customer.getLastname() + ".pdf"));
@@ -66,25 +72,25 @@ public class PdfServiceBean {
             
 	        Phrase receiverAddressGender = new Phrase();
 	        receiverAddressGender.add(new Chunk("Mr.", fontAddressBody));
-	        ColumnText.showTextAligned(canvas, Element.ALIGN_LEFT, receiverAddressGender, Utilities.millimetersToPoints(20), Utilities.millimetersToPoints(240), 0);
+	        ColumnText.showTextAligned(canvas, Element.ALIGN_LEFT, receiverAddressGender, Utilities.millimetersToPoints(22), Utilities.millimetersToPoints(240), 0);
 	        
 	        Phrase receiverAddressName = new Phrase();
 	        receiverAddressName.add(new Chunk(customer.getFirstname(), fontAddressBody));
 	        receiverAddressName.add(new Chunk(" ", fontAddressBody));
 	        receiverAddressName.add(new Chunk(customer.getLastname(), fontAddressBody));
-	        ColumnText.showTextAligned(canvas, Element.ALIGN_LEFT, receiverAddressName, Utilities.millimetersToPoints(20), Utilities.millimetersToPoints(236), 0);
+	        ColumnText.showTextAligned(canvas, Element.ALIGN_LEFT, receiverAddressName, Utilities.millimetersToPoints(22), Utilities.millimetersToPoints(236), 0);
 	        
 	        Phrase receiverAddressStreet = new Phrase();
 	        receiverAddressStreet.add(new Chunk(customer.getStreet(), fontAddressBody));
 	        receiverAddressStreet.add(new Chunk(" ", fontAddressBody));
 	        receiverAddressStreet.add(new Chunk(customer.getStreetNumber(), fontAddressBody));
-	        ColumnText.showTextAligned(canvas, Element.ALIGN_LEFT, receiverAddressStreet, Utilities.millimetersToPoints(20), Utilities.millimetersToPoints(232), 0);
+	        ColumnText.showTextAligned(canvas, Element.ALIGN_LEFT, receiverAddressStreet, Utilities.millimetersToPoints(22), Utilities.millimetersToPoints(232), 0);
             
 	        Phrase receiverAddressCity = new Phrase();
 	        receiverAddressCity.add(new Chunk(customer.getZipCode(), fontAddressBody));
 	        receiverAddressCity.add(new Chunk(" ", fontAddressBody));
 	        receiverAddressCity.add(new Chunk(customer.getCity(), fontAddressBody));
-	        ColumnText.showTextAligned(canvas, Element.ALIGN_LEFT, receiverAddressCity, Utilities.millimetersToPoints(20), Utilities.millimetersToPoints(228), 0);
+	        ColumnText.showTextAligned(canvas, Element.ALIGN_LEFT, receiverAddressCity, Utilities.millimetersToPoints(22), Utilities.millimetersToPoints(228), 0);
                  
 	        Phrase gmnyClaim = new Phrase();
 	        gmnyClaim.add(new Chunk("\"You can count on us\"", fontGmnyClaim));
@@ -119,7 +125,7 @@ public class PdfServiceBean {
 	        
 	        Calendar cal = Calendar.getInstance();
 	        String datef = ""+cal.get(Calendar.YEAR) + ", " + cal.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.ENGLISH) + " " + cal.get(Calendar.DAY_OF_MONTH);
-
+	        String dateg = ""+""+cal.get(Calendar.YEAR) + "-" + cal.get(Calendar.MONTH) + "-" + cal.get(Calendar.DAY_OF_MONTH);
 	        
 	        Phrase gmnyDate = new Phrase();
 	        gmnyDate.add(new Chunk(datef, fontGmnyAddress));
@@ -130,13 +136,71 @@ public class PdfServiceBean {
 	        logo.scaleToFit(Utilities.millimetersToPoints(43), Utilities.millimetersToPoints(43));
 	        logo.setAbsolutePosition(Utilities.millimetersToPoints(142), Utilities.millimetersToPoints(240));
 	        document.add(logo);
+
+	        Paragraph space = new Paragraph(Utilities.millimetersToPoints(115));
+	        document.add(space);
 	        
-	        document.add(new Paragraph("Your customer ID: " + customer.getId() + " (please state your customer ID whenever you contact us)", fontTextBold));
-	        document.add(new Paragraph(" ", fontText));
-	        document.add(new Paragraph(" ", fontText));
+	        document.add(Chunk.NEWLINE);
+	        document.add(new Paragraph("PRIVATE LOAN AGREEMENT", fontHeadline));
+	        document.add(Chunk.NEWLINE);
+	        document.add(Chunk.NEWLINE);
+	        document.add(new Paragraph("Your customer ID: " + customer.getId(), fontTextBold));
+	        document.add(Chunk.NEWLINE);
 	        document.add(new Paragraph("Dear Mr. " + customer.getLastname(), fontText));
-	        document.add(new Paragraph(" ", fontText));
-	        document.add(new Paragraph("We are pleased to inform you that your loan request has been accepted. We are willing to make the loan according to the terms and conditions set out below (the \"Loan\").", fontText));
+	        document.add(Chunk.NEWLINE);
+	        document.add(new Phrase("We are pleased to inform you that your loan request has been accepted. We are willing to make the loan according to the terms and conditions set out below. Please mail or fax us the signed agreement as soon as possible. If there are any questions left do not hesitate to contact us.", fontText));
+	        document.add(Chunk.NEWLINE);
+	        document.add(Chunk.NEWLINE);
+	        document.add(new Paragraph("Sincerely yours,", fontText));
+	        document.add(Chunk.NEWLINE);
+	        document.add(Chunk.NEWLINE);
+	        document.add(new Paragraph("GMNY loan office", fontText));
+	        document.add(Chunk.NEWLINE);
+	        document.add(Chunk.NEWLINE);
+	        document.add(Chunk.NEWLINE);
+	        document.add(new Paragraph("Please find attached:", fontTextUnderline));
+	        document.add(Chunk.NEWLINE);
+	        document.add(new Paragraph(" - Private Loan Agreement", fontText));
+	        
+	        //Contract
+	        
+	        document.add(Chunk.NEXTPAGE);
+	        
+	        Paragraph hp0 = new Paragraph("PRIVATE LOAN AGREEMENT", fontTextMassive);
+	        hp0.setAlignment(Element.ALIGN_CENTER);
+	        document.add(hp0);
+	        
+	        canvas.setLineWidth(2.0f);
+	        canvas.moveTo(Utilities.millimetersToPoints(20), Utilities.millimetersToPoints(255));
+	        canvas.lineTo(Utilities.millimetersToPoints(190), Utilities.millimetersToPoints(255));
+	        canvas.stroke();
+	        
+	        document.add(Chunk.NEWLINE);
+	        
+	        document.add(new Phrase("The Private loan Agreement (\"The agreement\") shall be governed by the laws of the Federal Republic of Germany. The agreement is made and will be effective on ", fontText));
+	        document.add(new Phrase(dateg, fontTextBold));
+	        document.add(Chunk.NEWLINE);
+	        document.add(Chunk.NEWLINE);
+	        
+	        Paragraph hp1 = new Paragraph("B E T W E E N", fontHeadline);
+	        hp1.setAlignment(Element.ALIGN_CENTER);
+	        document.add(hp1);
+	        document.add(Chunk.NEWLINE);
+	        
+	        Paragraph hp2 = new Paragraph(customer.getFirstname() + " " + customer.getLastname() + ", " + customer.getStreet() + " " + customer.getStreetNumber() + ", " + customer.getZipCode() + " " + customer.getCity() + "   (\"The Borrower\")", fontTextBold);
+	        hp2.setAlignment(Element.ALIGN_CENTER);
+	        document.add(hp2);
+	        
+	        document.add(Chunk.NEWLINE);
+	        
+	        Paragraph hp3 = new Paragraph("A N D", fontHeadline);
+	        hp3.setAlignment(Element.ALIGN_CENTER);
+	        document.add(hp3);
+	        document.add(Chunk.NEWLINE);
+	        
+	        Paragraph hp4 = new Paragraph("GMNY direct AG, Lothar-Matthäus-Platz 2, 48149 Münster   (\"The Lender\").", fontTextBold);
+	        hp4.setAlignment(Element.ALIGN_CENTER);
+	        document.add(hp4);
 	        
             document.close();
             System.out.println("PDF PDF PDF");
