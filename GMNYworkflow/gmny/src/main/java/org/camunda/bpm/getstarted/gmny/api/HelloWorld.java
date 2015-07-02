d1c48c7812e6ea49618511ee39e9981ab6b87ff5
ChrisJim <christoph.wemmer@gmail.com>
2015-07-02 20:38:51 +0200

    Revert "Revert "Api impl""

---
+++ GMNYworkflow/gmny/src/main/java/org/camunda/bpm/getstarted/gmny/api/HelloWorld.java
@@ -0,0 +1,60 @@
+package org.camunda.bpm.getstarted.gmny.api;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+
+import org.camunda.bpm.getstarted.gmny.model.CustomerEntity;
+
+@Path("")
+public class HelloWorld {
+	
+	@PersistenceContext
+	private EntityManager entityManager;
+
+    @GET
+    @Path("/helloworld")
+    public Response getHelloWorld() {
+
+        String value = "Hello World - YEAH";
+        return Response.status(200).entity(value).build();
+    }
+    
+    @GET
+    @Path("/getCustomer/{id}")
+    @Produces("application/json")
+    public CustomerEntity getCustomer(@PathParam("id") long customerId) {
+    	
+    	CustomerEntity customer = entityManager.find(CustomerEntity.class, customerId);
+    	
+    	return customer;
+    }
+    
+    /*
+    
+    //pw check could be implemented like this...
+    @GET
+    @Path("/checkPassword/{id}/{pw}")
+    public Response checkPassword(@PathParam("id") long customerId, @PathParam("pw") String password) {
+    	
+    	
+    	String value = "Hello World - YEAH";
+    	CustomerEntity customer = entityManager.find(CustomerEntity.class, customerId);
+    	
+    	value = "fail";
+    	
+    	if (customer.getPassword() == password){
+    		value = "pass";
+    	}
+    	
+    	return Response.status(200).entity(value).build();
+    }
+    */
+    
+}
---
+++ GMNYworkflow/gmny/src/main/java/org/camunda/bpm/getstarted/gmny/application/BaseApplication.java
@@ -0,0 +1,17 @@
+package org.camunda.bpm.getstarted.gmny.application;
+
+import java.util.Collections;
+import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("")
+public class BaseApplication extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+
+        return Collections.emptySet();
+    }
+}
